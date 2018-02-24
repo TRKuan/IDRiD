ccb2a3413614ed966463db3e029ac62243b56fa6
TR <tianruei.kuan@gmail.com>
2018-02-25 02:58:54 +0800

    training code beeing tested on small dataset

--- train.py
+++ train.py
@@ -1,97 +1,113 @@
 # -*- coding: utf-8 -*
 from dataset import IDRiD_sub1_dataset
-from util import CrossEntropyLoss2d, save_model
+from util import evaluate, save_model
 from model import Model
 import torch
 import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-import numpy as np
 import time
+import copy
 
 use_gpu = torch.cuda.is_available
 save_dir = "./saved_models"
+model_name = "net.pth"
 data_train_dir = './data/sub1/train'
 data_val_dir = './data/sub1/val'
-task_type = 'MA'
-batch_size = 2
+batch_size = 4
 num_epochs = 10
+lr = 1e-4
 
 def make_dataloaders(batch_size=batch_size):
-    dataset_train = IDRiD_sub1_dataset(data_train_dir, task_type)
-    #dataset_val = IDRiD_sub1_dataset(data_val_dir, task_type)
-    dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=4)
-    #dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=True, num_workers=4)
-    #dataloaders = {'train': dataloader_train, 'val': dataloader_val}
-    #print('Training data: %d\nValidation data: %d'%(len(dataset_train)), len(dataset_val))
-    dataloaders = {'train': dataloader_train}
-    print('Training data: %d'%(len(dataset_train)))
+    dataset_train = IDRiD_sub1_dataset(data_train_dir)
+    dataset_val = IDRiD_sub1_dataset(data_val_dir)
+    dataloader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=False, num_workers=4)
+    dataloader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=False, num_workers=4)
+    dataloaders = {'train': dataloader_train, 'val': dataloader_val}
+    print('Training data: %d\nValidation data: %d'%((len(dataset_train)), len(dataset_val)))
     return dataloaders
 
 def train_model(model, num_epochs, dataloaders, criterion, optimizer):
     since = time.time()
-    best_model_wts = model.state_dict()
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
     
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
         
-        #for phase in ['train', 'val']:
-        for phase in ['train']:
+        for phase in ['train', 'val']:
             if phase == 'train':
                 model.train(True)  # Set model to training mode
             else:
                 model.train(False)  # Set model to evaluate mode
         
-        running_loss = 0.0
-        data_num = 0
-        
-        for idx, data in enumerate(dataloaders[phase]):
-            images, masks, names = data
-
-            if use_gpu:
-                images = images.cuda()
-                masks = masks.cuda()
-            if phase == 'train':
-                images, masks = Variable(images, volatile=False), Variable(masks, volatile=False)
-            else:
-                images, masks = Variable(images, volatile=True), Variable(masks, volatile=True)
-
-            optimizer.zero_grad()
+            running_loss = 0.0
+            running_acc = 0.0
+            data_num = 0
             
-            #forward
+            for idx, data in enumerate(dataloaders[phase]):
+                images, masks, names = data
+    
+                if use_gpu:
+                    images = images.cuda()
+                    masks = masks.cuda()
+                if phase == 'train':
+                    images, masks = Variable(images, volatile=False), Variable(masks, volatile=False)
+                else:
+                    images, masks = Variable(images, volatile=True), Variable(masks, volatile=True)
+    
+                optimizer.zero_grad()
+                
+                #forward
+                
+                outputs = model(images)
+                loss = criterion(outputs, masks)
+                
+                #backword
+                
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+                
+                # statistics
+                running_loss += loss.data[0] * images.size(0)
+                data_num += images.size(0)
+                
+                outputs = F.sigmoid(outputs).cpu().data#remenber to apply sigmoid befor usage
+                masks = masks.cpu().data
+                for i in range(len(outputs)):
+                    y_pred = outputs[i]
+                    y_true = masks[i]
+                    running_acc += evaluate(y_pred, y_true)
+                
+                #verbose
+                if idx%5==0 and idx!=0:
+                    print('\r{} {:.2f}%'.format(phase, 100*idx/len(dataloaders[phase])), end='')
+     
+            print()
             
-            outputs = model(images)
-            loss = criterion(outputs, masks)
+            epoch_loss = running_loss / data_num
+            epoch_acc = running_acc / data_num
+            print('{} Loss: {:.4f} Accuracy: {:.4f}'.format(phase, epoch_loss, epoch_acc))
             
-            #backword
-            
-            if phase == 'train':
-                loss.backward()
-                optimizer.step()
-            
-            # statistics
-            
-            running_loss += loss.data[0] * images.size(0)
-            data_num += images.size(0)
-            
-        
-        epoch_loss = running_loss / data_num
-        print('{} Loss: {:.4f}'.format(phase, epoch_loss))
-        '''
-        # deep copy the model
+            # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
-        '''
-    print()
+                save_model(model, save_dir, model_name)
+        
+        print()
+    
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
-    #print('Best val Acc: {:4f}'.format(best_acc))
+    print('Best accuracy: {:.4f}'.format(best_acc))
 
     # load best model weights
-    #model.load_state_dict(best_model_wts)
+    model.load_state_dict(best_model_wts)
     return model
 
 if __name__ == '__main__':
@@ -100,11 +116,13 @@ if __name__ == '__main__':
     model = Model()
     if use_gpu:
         model = model.cuda()
+        #model = torch.nn.DataParallel(model).cuda()
     
-    criterion = CrossEntropyLoss2d()
-    optimizer = optim.Adam(model.parameters(), lr = 0.001)
-    
+    #training
+    criterion = nn.BCEWithLogitsLoss(size_average=True)
+    optimizer = optim.Adam(model.parameters(), lr = lr)
     model = train_model(model, num_epochs, dataloaders, criterion, optimizer)
-    save_model(model, save_dir, "net.pth")
+    
+    save_model(model, save_dir, model_name)
     
     
--- util.py
+++ util.py
@@ -1,41 +1,35 @@
 # -*- coding: utf-8 -*
 import torch
-import torch.nn.functional as F
-import torch.nn as nn
+from sklearn.metrics import accuracy_score
+import numpy as np
 import os
 
-
-class CrossEntropyLoss2d(nn.Module):
-
-    def __init__(self, size_average=True, ignore_label=255):
-        super(CrossEntropyLoss2d, self).__init__()
-        self.size_average = size_average
-        self.ignore_label = ignore_label
-
-    def forward(self, predict, target, weight=None):
-        """
-            Args:
-                predict:(n, c, h, w)
-                target:(n, h, w)
-                weight (Tensor, optional): a manual rescaling weight given to each class.
-                                           If given, has to be a Tensor of size "nclasses"
-        """
-        assert not target.requires_grad
-        assert predict.dim() == 4
-        assert target.dim() == 3
-        assert predict.size(0) == target.size(0), "{0} vs {1} ".format(predict.size(0), target.size(0))
-        assert predict.size(2) == target.size(1), "{0} vs {1} ".format(predict.size(2), target.size(1))
-        assert predict.size(3) == target.size(2), "{0} vs {1} ".format(predict.size(3), target.size(3))
-        n, c, h, w = predict.size()
-        target_mask = (target >= 0) * (target != self.ignore_label)
-        target = target[target_mask]
-        predict = predict.transpose(1, 2).transpose(2, 3).contiguous()
-        predict = predict[target_mask.view(n, h, w, 1).repeat(1, 1, 1, c)].view(-1, c)
-        loss = F.cross_entropy(predict, target, weight=weight, size_average=self.size_average)
-        return loss
+def evaluate(y_pred, y_true):
+    '''
+    Calculate statistic matrix.
     
+    Args:
+        y_pred:the pytorch tensor of prediction
+        y_true:the pytorch tensor of ground truth
+    '''
+    y_pred = np.rint(y_pred.numpy().flatten())
+    y_true = y_true.numpy().flatten()
+    acc = accuracy_score(y_true, y_pred)
+    return acc
+    '''
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+    accuracy = (tp+tn)/(tp+tn+fp+fn)
+    if tp+fn == 0: sensitivity = 0.0
+    else: sensitivity = tp/(tp+fn)
+    specificity = tn/(fp+tn)
+    ppv = tp/(tp+fp)
+    return sensitivity, specificity, ppv, accuracy
+    '''
+
 def save_model(model, save_dir, name):
     #save model
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     torch.save(model.state_dict(), os.path.join(save_dir, name))
+
+
